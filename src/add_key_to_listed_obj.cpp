bd4afdbd516d7050a45cd5a4976a35530a761772
Matthias Bannert <bannert@kof.ethz.ch>
2013-09-29 21:33:37 +0200

    fixed add_key_to_list to iterate over char vector too

--- src/add_key_to_listed_obj.cpp
+++ src/add_key_to_listed_obj.cpp
@@ -9,14 +9,29 @@ using namespace Rcpp;
 //'  @param y character vector of the same length as x
 //'
 // [[Rcpp::export]]
-RObject add_key_to_listed_obj (List x, CharacterVector y){
-  List out = x;
+RObject add_key_to_listed_obj(List x, CharacterVector y){
+  List out = clone(x);
   int n = x.size();
+  CharacterVector val;
   
   for(int i = 0; i < n; ++i) {
     RObject el = out[i];
-    el.attr("mi_key") = y;
+    val = y[i];
+    el.attr("testkey") = val; 
   }
  
   return out;
 }
+ 
+/*** R
+
+tlist <- as.list(1:1000)
+for (i in 1:length(tlist)){
+  
+  tlist[[i]] <- ts(rnorm(1:100),start=c(1999,2),frequency=4)
+}
+names(tlist) <- paste("key",1:1000,sep="_")
+
+tlist.mod <- add_key_to_listed_obj(tlist,names(list))
+ 
+*/
