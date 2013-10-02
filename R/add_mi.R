903eb06bf34c49c743748e6e0e90d71925ca22d9
Matthias Bannert <bannert@kof.ethz.ch>
2013-10-02 09:15:44 +0200

    +removed data fields from class definition, updated old md fields

--- R/add_mi.R
+++ R/add_mi.R
@@ -7,9 +7,3 @@ add_mi <- function(x,meta_env_name = "meta"){
   assign(attributes(x)$mi_key,meta_obj,envir=get(meta_env_name))
   
 }
-
-create_env <- function(nm){
-  e <- new.env()
-  assign(nm,e,envir = .GlobalEnv)
-  cat("Meta environment",nm,"created.")
-}
--- R/mi_ts_class_defintion.R
+++ R/mi_ts_class_defintion.R
@@ -11,7 +11,7 @@ mi_ts <- setRefClass("mi_ts",fields = list(ts_key = "mi_ts_key",
                                            ts_localized_meta = "metaLocalized"),
                      methods = list(
                           addComment = function(cmnt){
-                            md_comment <<- cmnt
+                            ts_comment <<- cmnt
                           },
                           addKey = function(cntry,prov,src,level,
                                             slevel,var,itm){
@@ -27,11 +27,11 @@ mi_ts <- setRefClass("mi_ts",fields = list(ts_key = "mi_ts_key",
                                                      slevel,var,itm,sep=".")
                           },
                           addLegacy = function(key,src){
-                            md_legacy_key <<- key
-                            md_source <<- src
+                            ts_legacy_key <<- key
+                            ts_source <<- src
                           },
                           addRestrictions = function(restrict){
-                            md_restrictions <<- restrict
+                            ts_restrictions <<- restrict
                           },
                           addLocalizedMeta = function(kvp){
                             # sanity checks
@@ -44,25 +44,25 @@ mi_ts <- setRefClass("mi_ts",fields = list(ts_key = "mi_ts_key",
                             if((lang %in% md_localized_meta$languages)){
                               cat("Updated existing language.")
                               # check whether this fields already exist
-                              ex <- (names(kvp) %in% names(md_localized_meta$labels[[lang]]))
+                              ex <- (names(kvp) %in% names(ts_localized_meta$labels[[lang]]))
                               # new information
                               ni <- kvp[!ex]
-                              md_localized_meta$labels[[lang]] <<- c(md_localized_meta$labels[[lang]],ni)
+                              ts_localized_meta$labels[[lang]] <<- c(ts_localized_meta$labels[[lang]],ni)
                               
                             } else {
                               # append language
-                              md_localized_meta$languages <<- c(md_localized_meta$languages,lang)
-                              md_localized_meta$labels[[lang]] <<- kvp
+                              ts_localized_meta$languages <<- c(ts_localized_meta$languages,lang)
+                              ts_localized_meta$labels[[lang]] <<- kvp
                             }
                           },
                           getMeta = function(lang="de"){
-                            md_localized_meta$labels[[lang]]
+                            ts_localized_meta$labels[[lang]]
                           },
-                          ggplot = function(...){
-                            g <- ggplot2::ggplot(ts_data,ggplot2::aes(Date,value),...)
-                            g + ggplot2::geom_point() + ggplot2::geom_line() +
-                              ggplot2::ylab(ts_key$fullKey)
-                          },
+#                           ggplot = function(...){
+#                             g <- ggplot2::ggplot(ts_data,ggplot2::aes(Date,value),...)
+#                             g + ggplot2::geom_point() + ggplot2::geom_line() +
+#                               ggplot2::ylab(ts_key$fullKey)
+#                           },
                           populateFromWorkspace = function(tsObj,
                                                            nm = NA_character_,
                                                            comment = character(),
@@ -77,76 +77,23 @@ mi_ts <- setRefClass("mi_ts",fields = list(ts_key = "mi_ts_key",
                               key$setKey(nm)
                               ts_key <<- key
                               
-                              # ts data
-                              data <- as.matrix(tsObj)
-                              data <- data.frame(Date=.zoolike.Date.convert(tsObj),
-                                                 value=data,stringsAsFactors=F)
-                              
-                              ts_data <<- as.data.frame(data)
-                              ts_index <<- .zoolike.Date.convert(tsObj)
-                              
                               ts_frequency <<- frequency(tsObj)
                               
                               # meta information on users and time
-                              md_generated_on <<- as.character(Sys.Date())
-                              md_generated_by <<- Sys.getenv("USER")
+                              ts_generated_on <<- as.character(Sys.Date())
+                              ts_generated_by <<- Sys.getenv("USER")
                               
                               # get source information from key
-                              md_source <<- ts_key$src
+                              ts_source <<- ts_key$src
                               
                               # comments and restrictiions
-                              md_comment <<- comment
-                              md_restrictions <<- restrictions
+                              ts_comment <<- comment
+                              ts_restrictions <<- restrictions
                               
                               
                             } else {
                               stop("Input is not of valid class. Only ts and zoo are supported.")
                             }
-                          },
-                          show = function(){
-                            methods::show(ts_key)
-                            methods::show(head(ts_data))
-                            if(nrow(ts_data) != 0){
-                              cat("Showing 6 out of",nrow(ts_data),
-                                  "rows. Access $ts_data to get the entire data.frame. \n")
-                              cat("Use $getMeta() to access localized meta information: ",
-                                  md_localized_meta$languages,"\n") 
-                            }
-                            if(!(length(md_restrictions) == 0)){
-                              cat("!! DATASET IS RESTRICTED:",md_restrictions)
-                              cat("\n")
-                            }
-                          },
-                          showAll = function(){
-                            show()
-                            cat("Index:")
-                            methods::show(ts_index)
-                            cat("\n")
-                            cat("Frequency: ")
-                            methods::show(ts_frequency)
-                            cat("\n")
-                            cat("Meta information added on: ")
-                            methods::show(md_generated_on)
-                            cat("\n")
-                            cat("last editor:")
-                            methods::show(md_generated_by)
-                            cat("\n")
-                            cat("Comments: ")
-                            methods::show(md_comment)
-                            cat("\n")
-                            cat("RESTRICTIONS: ")
-                            methods::show(md_restrictions)
-                          },
-                          tsOut = function(){
-                            s <- c(as.numeric(strftime(ts_index,"%Y")[1]),
-                                   as.numeric(strftime(ts_index,"%m")[1])
-                            )
-                            out <- ts(ts_data$value,start = s,frequency = ts_frequency)
-                            out
-                          },
-                          zooOut = function(){
-                            z <- zoo::zoo(ts_data$value,
-                                          zoo::as.yearqtr(ts_data$Date))
-                            z
-                          })
+                          }
+                          )
 )
